9edc90bd17033e2660858506ed498f5c10ddce38
Soumyashree6002 <soumyashree6002@gmail.com>
2025-03-03 16:18:40 +0530

    Added the js for retrieving books and showing in html

---
+++ static/dashboard.js
@@ -0,0 +1,56 @@
+let currentPage = 1;
+let limit = 5;
+
+async function fetchBooks() {
+    let genre = document.getElementById("genreFilter").value;
+    let rating = document.getElementById("ratingFilter").value;
+    limit = document.getElementById("limitSelect").value;
+
+    let url = `/books?page=${currentPage}&limit=${limit}`;
+    if(genre) {
+        url += `&genre=${genre}`;
+    }
+    if(rating) {
+        url += `&rating=${rating}`;
+    }
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        let tableBody = document.getElementById("tableBody");
+        tableBody.innerHTML = "";
+
+        data.books.forEach(book => {
+            let row = `<tr>
+                    <td>${book.title}</td>
+                    <td>${book.author}</td>
+                    <td>${book.genre}</td>
+                    <td>${book.rating || "N/A"}</td>
+                </tr>`;
+            tableBody.innerHTML += row;
+        });
+    } 
+    catch (error) {
+        console.error("Error fetching books : ", error);
+    }
+}
+
+function nextPage() {
+    currentPage++;
+    fetchBooks();
+}
+
+function prevPage() {
+    if(currentPage > 1) {
+        currentPage--;
+    }
+    fetchBooks();
+}
+
+function updateLimit() {
+    currentPage = 1;
+    fetchBooks();
+}
+
+window.onload = fetchBooks;
